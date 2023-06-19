02a8e2b0283bb2eb23f2d1210ec935429dfa8a85
a.ayman <a.ayman@DESKTOP-0DI1A6N>
2023-06-19 17:16:20 +0300

    Add ExceptionMiddleware and finsh error handling

---
+++ Talabaty.API/Errors/ApiException.cs
@@ -0,0 +1,16 @@
+﻿namespace Talabaty.API.Errors
+{
+    public class ApiException : ApiResponse
+    {
+        public ApiException(int statusCode, string message = null, string details = null): base(statusCode, message)
+        {
+
+        }
+        public string Details { get; set; }
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+
+    }
+}
--- Talabaty.API/Program.cs
+++ Talabaty.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Talabaty.API.Middleware;
 using Talabaty.BLL.Interfaces;
 using Talabaty.BLL.Repositories;
 using Talabaty.DAL.Data;
@@ -30,7 +31,6 @@ namespace Talabaty.API
 
 
 
-
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -43,6 +43,8 @@ namespace Talabaty.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
