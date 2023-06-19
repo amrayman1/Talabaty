575746f0ab3d3175281d9bd6caa1f8a6b41f3e59
a.ayman <a.ayman@DESKTOP-0DI1A6N>
2023-06-19 17:41:09 +0300

    adding a sorting specification class part 1

--- Talabaty.API/Controllers/ErrorController.cs
+++ Talabaty.API/Controllers/ErrorController.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Talabaty.API.Errors;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Talabaty.API.Errors;
 
-namespace Talabaty.API.Controllers
-{
-    [Route("error/{code}")]
-    public class ErrorController : BaseApiController
-    {
-        public IActionResult Error(int code)
-        {
-            return new ObjectResult(new ApiResponse(code));
-        }
-    }
-}
+//namespace Talabaty.API.Controllers
+//{
+//    [Route("error/{code}")]
+//    public class ErrorController : BaseApiController
+//    {
+//        public IActionResult Error(int code)
+//        {
+//            return new ObjectResult(new ApiResponse(code));
+//        }
+//    }
+//}
--- Talabaty.BLL/Specifications/BaseSpecification.cs
+++ Talabaty.BLL/Specifications/BaseSpecification.cs
@@ -21,9 +21,24 @@ namespace Talabaty.BLL.Specifications
 
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
         protected void AddIncludes(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
         }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderExpression)
+        {
+            OrderBy = orderExpression;
+        }
+        
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+
     }
 }
